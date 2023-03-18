6048bb8210dea41ee3d3974a20522e8db313104d
mrbilby <baileymrb@gmail.com>
2023-03-18 19:28:04 +0100

    Basic working file

--- WeTemp/ContentView.swift
+++ WeTemp/ContentView.swift
@@ -8,14 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @State private var tempAmount = 0.0
+    @State private var measSelect = "Celsius"
+    @State private var measChoice = "Fahrenheit"
+    @FocusState private var amountIsFocused: Bool
+    let measurement = ["Celsius", "Kelvin", "Fahrenheit"]
+
+    var conversion: Double {
+        var tempConv = 1.1
+        if measSelect == "Celsius" && measChoice == "Fahrenheit" {
+            tempConv = (tempAmount*(9/5))+32
+        } else if measSelect == "Fahrenheit" && measChoice == "Celsius" {
+            tempConv = (tempAmount-32)*(5/9)
+        } else if measSelect == "Celsius" && measChoice == "Kelvin" {
+            tempConv = tempAmount + 273.15
+        } else if measSelect == "Fahrenheit" && measChoice == "Kelvin"{
+            tempConv = (tempAmount-32)*(5/9) + 273.15
+        } else if measSelect == "Kelvin" && measChoice == "Celsius" {
+            tempConv = tempAmount - 273.15
+        } else if measSelect == "Kelvin" && measChoice == "Fahrenheit" {
+            tempConv = ((tempAmount - 273.15)*(9/5)) + 32
+        } else {
+            tempConv = tempAmount
         }
-        .padding()
+        
+        return tempConv
+    }
+
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $tempAmount, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    Picker("Measurement", selection: $measSelect) {
+                        ForEach(measurement, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                } header: {
+                    Text("Temperature")
+                }
+                Section {
+                    Picker("Measurement", selection: $measChoice) {
+                        ForEach(measurement , id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                } header: {
+                    Text("What do you want to convert to?")
+                }
+                Section {
+                    Text("\(conversion, format: .number) \(measChoice)")
+                } header: {
+                    Text("Conversion")
+                }
+            }
+            .navigationTitle("WeTemp")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
     }
 }
 
