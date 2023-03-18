d877900d26fa3e4ab6b24fd38a5245c6cd7051ad
mrbilby <baileymrb@gmail.com>
2023-03-18 18:18:22 +0100

    Initial Commit

---
+++ WeTemp/WeTempApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeTempApp.swift
+//  WeTemp
+//
+//  Created by James Bailey on 18/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeTempApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
